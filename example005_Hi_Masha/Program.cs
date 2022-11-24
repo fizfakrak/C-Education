c650538dc0206212fd92caec2c6f21d08adb312e
fizfakrak <anton79910161@yandex.ru>
2022-11-24 11:22:22 +0500

    add special Hi to Masha

---
+++ example005_Hi_Masha/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("УРА, это Маша!");
+}
+else
+{
+    Console.Write("Хай, ");
+    Console.WriteLine(username);
+}

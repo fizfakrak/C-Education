552a4897d376564b435da9046d7c68e9517a2640
fizfakrak <anton79910161@yandex.ru>
2022-11-25 23:33:30 +0500

    add task 012

---
+++ Task012/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите первое число ");
+int input_number_1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int input_number_2 = int.Parse(Console.ReadLine());
+if(input_number_1%input_number_2 == 0)Console.WriteLine("Кратно");
+else Console.WriteLine($"Не кратно, остаток {input_number_1%input_number_2}");

6cc7934ff5cb3a27c1f4e436c5c006f70397cbe2
fizfakrak <anton79910161@yandex.ru>
2022-11-25 22:58:22 +0500

    add task 009

---
+++ Task009/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+int input_number = new Random().Next(10,100);
+Console.WriteLine(input_number);
+if(input_number % 10 > input_number / 10) Console.WriteLine(input_number % 10);
+else Console.WriteLine(input_number / 10);

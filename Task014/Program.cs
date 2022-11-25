61afd61f463399c28f9b9ca5b66de02bc663906f
fizfakrak <anton79910161@yandex.ru>
2022-11-25 23:42:01 +0500

    add task 014

---
+++ Task014/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int input_number = int.Parse(Console.ReadLine());
+if(input_number % 7 == 0 && input_number % 23 == 0)Console.Write("Число кратно 7 и 23");
+else Console.Write("Число некратно 7 и/или 23");

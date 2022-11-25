621d575c903fd4aa735096b47fea456328b2de52
fizfakrak <anton79910161@yandex.ru>
2022-11-25 13:07:33 +0500

    add method and array

---
+++ example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+int[] array = {12, 99, 37, 54, 43, 64, 66, 21};
+int arrayLength = array.Length;
+int find = 64;
+
+int index = 0;
+
+while(index < arrayLength)
+{
+    if(find == array[index])
+    {
+        Console.WriteLine($"Index find = {index}");
+        break;
+    }
+    index++;
+}

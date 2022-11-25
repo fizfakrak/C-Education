a25244033bc472a65d7eacb6d98f619cb70b1e95
fizfakrak <anton79910161@yandex.ru>
2022-11-25 23:21:42 +0500

    add task 011

---
+++ Task011/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+int input_number = new Random().Next(100,1000);
+int output_number = input_number/100*10 + input_number%10;
+Console.WriteLine($"Случайное число: {input_number}");
+Console.Write($"Вывод: {output_number}");

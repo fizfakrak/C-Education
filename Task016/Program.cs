5f2e595d1f1e2e04f62a5f12204ea3cdde1e6a15
fizfakrak <anton79910161@yandex.ru>
2022-11-26 00:11:46 +0500

    add task 016

---
+++ Task016/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите первое число ");
+int input_number_1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int input_number_2 = int.Parse(Console.ReadLine());
+if(input_number_1 / input_number_2 == input_number_2 || input_number_2 / input_number_1 == input_number_1)Console.Write("yes");
+else Console.Write("no");

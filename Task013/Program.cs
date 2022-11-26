fc33b20477094c24f5157c055551b5f8f411be5b
fizfakrak <anton79910161@yandex.ru>
2022-11-26 18:27:49 +0500

    add task 013

---
+++ Task013/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введитее число: ");
+int input_number = int.Parse(Console.ReadLine());
+if(input_number < 100) Console.Write($"Третьей цифры нет");
+else
+{    while (input_number > 1000)
+    {
+        input_number = input_number/10;
+    }
+    input_number = input_number % 10;
+    Console.Write($"Третья цифра: {input_number}");
+}

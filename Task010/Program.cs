4ed9f173efb461ea8882a0a530438ee1a9184883
fizfakrak <anton79910161@yandex.ru>
2022-11-26 18:03:20 +0500

    add task 010

--- Task010/Program.cs
+++ Task010/Program.cs
@@ -1,1 +1,10 @@
 ﻿Console.Clear();
+Console.Write("Введитее трехзначное число: ");
+int input_number = int.Parse(Console.ReadLine());
+while((input_number > 999)||(input_number < 100))
+{
+    Console.Write("Введитее трехзначное число: ");
+    input_number = int.Parse(Console.ReadLine());
+}
+input_number = (input_number/10)%10;
+Console.Write($"Вторая цифра: {input_number}");

b84634109622120431bf5c6c00942c0b02344cc6
fizfakrak <anton79910161@yandex.ru>
2022-12-05 20:49:43 +0500

    add hometask

---
+++ Task019/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int input_number = int.Parse(Console.ReadLine());
+while((input_number<9999) || (input_number>100000))
+{
+Console.Write("Введите пятизначное число: ");
+input_number = int.Parse(Console.ReadLine());
+}
+if((input_number/10000 == input_number%10) && ((input_number/1000)%10 == (input_number/10)%10))
+{
+    Console.Write("Число является палиномом");
+}
+else Console.Write("Число не является палиномом");
---
+++ Task023/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int input_number = int.Parse(Console.ReadLine());
+int counter = 1;
+double result = 0;
+Console.Write("Список квадратов: ");
+while(counter<=input_number)
+{
+result = Math.Pow(counter, 2);
+Console.Write($"{result}, ");
+counter++;
+}

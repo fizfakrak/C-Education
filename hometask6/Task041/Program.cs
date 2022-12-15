3299ff5822178c6a3e12a23b03c967b1e7c1ffd2
fizfakrak <anton79910161@yandex.ru>
2022-12-15 21:33:10 +0500

    add hometask6

---
+++ hometask6/Task041/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+int length = new Random().Next(10,21);
+int[] array = new int [length];
+int counter = 0;
+int number_counter = 0;
+for(counter = 0; counter < array.Length; counter++)
+{
+    Console.Write($"Введите {counter} число в массив = ");
+    array[counter] = int.Parse(Console.ReadLine());
+    if(array[counter] > 0) number_counter++;
+}
+Console.Write($"Количество чисел больше нуля = {number_counter} ");
---
+++ hometask6/Task043/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+double x=0,y=0;
+Console.Write($"Введите k1 = ");
+double k1 = double.Parse(Console.ReadLine());
+Console.Write($"Введите b1 = ");
+double b1 = double.Parse(Console.ReadLine());
+Console.Write($"Введите k2 = ");
+double k2 = double.Parse(Console.ReadLine());
+Console.Write($"Введите b2 = ");
+double b2 = double.Parse(Console.ReadLine());
+
+x = (b2-b1)/(k1-k2);
+y = x*k1+b1;
+Console.WriteLine($"x = {x} ");
+Console.WriteLine($"y = {y} ");
